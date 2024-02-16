ef219f33c5c11e48c1ad61b4a79c6a87f19a692d
pipefellow <travislistak@gmail.com>
2024-02-15 22:48:23 -0700

    article index work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def rich_to_plain_without_images(content)
     content.to_plain_text.gsub(/[\[].*[\]]/, "").gsub(/\\n/, " ").truncate(1000)
   end
+
+  def humanize_date(date)
+    date.strftime("%d %b, %y")
+  end
 end
