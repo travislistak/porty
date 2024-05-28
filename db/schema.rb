e7cccb7814377f7734e0bac628a872b3a6280e8e
pipefellow <travislistak@gmail.com>
2024-05-27 23:53:30 -0600

    Remove unused tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_17_030909) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_28_055116) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,28 +50,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_17_030909) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "article_types", force: :cascade do |t|
-    t.string "category"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "published", null: false
-    t.bigint "article_type_id", null: false
-    t.index ["article_type_id"], name: "index_articles_on_article_type_id"
-  end
-
-  create_table "offlines", force: :cascade do |t|
-    t.string "title"
-    t.boolean "published"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "projects", force: :cascade do |t|
