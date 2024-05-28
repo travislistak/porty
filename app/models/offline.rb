7ba54a58d8aff21a7d7c6dfa2083ed22a0cdf835
pipefellow <travislistak@gmail.com>
2024-05-28 00:29:30 -0600

    make 404 page

--- app/models/offline.rb
+++
@@ -1,9 +0,0 @@
-class Offline < ApplicationRecord
-  has_rich_text :content
-  has_one_attached :header_image do |attachable|
-    attachable.variant :thumbnail, resize: "100x100"
-    attachable.variant :medium, resize: ""
-  end
-
-  default_scope { where(published: true).order(created_at: :desc) }
-end
